0de688ccdcc40324fe830f4f91b05641d243d8a2
XUYIHANG <xyhaiwsq@outlook.com>
2020-07-09 00:34:06 +0900

    DbHelperMySQL.csの内容を完成できた

--- LOLDemo/MyLoLServer/Dao/DbHelperMySQL.cs
+++ LOLDemo/MyLoLServer/Dao/DbHelperMySQL.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Collections;
 
 namespace Maticsoft.DBUtility
 {
@@ -96,9 +97,202 @@ namespace Maticsoft.DBUtility
 
         #region 簡単なSQLメソッド
 
+        /// <summary>
+        /// ①SQL言語実行、影響された記録の数を戻す
+        /// </summary>
+        /// <param name="SQLString">SQL言語</param>
+        /// <returns></returns>
+        public static int ExecuteSql(string SQLString)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        return rows;
+                    }
+                    catch(MySql.Data.MySqlClient.MySqlException e)
+                    {
+                        connection.Close();
+                        throw e;
+                    }
+                }
+            }
+        }
 
         /// <summary>
-        /// メソッド一つ実行すると、Object結果を戻す
+        /// ②SQL言語実行、影響された記録の数を戻す(base of time)
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="Times"></param>
+        /// <returns></returns>
+        public static int ExecuteSqlByTime(string SQLString ,int Times)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        cmd.CommandTimeout = Times;
+                        int rows = cmd.ExecuteNonQuery();
+                        return rows;
+                    }
+                    catch(MySql.Data.MySqlClient.MySqlException e)
+                    {
+                        connection.Close();
+                        throw e;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ③多数のSQL同時実行、業務処理
+        /// </summary>
+        /// <param name="SQLStringList"></param>
+        /// <returns></returns>
+        public static int ExecuteSqlTran(List<String> SQLStringList)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                MySqlTransaction tx = conn.BeginTransaction();
+                cmd.Transaction = tx;
+                try
+                {
+                    int count = 0;
+                    for(int n = 0;n<SQLStringList.Count;n++)
+                    {
+                        string strsql = SQLStringList[n];
+                        if(strsql.Trim().Length>1)
+                        {
+                            cmd.CommandText = strsql;
+                            count += cmd.ExecuteNonQuery();
+                        }
+                    }
+                    tx.Commit();
+                    return count;
+                }
+                catch
+                {
+                    tx.Rollback();
+                    return 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ④保存した引数付きのSQLを実行
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="content">引数の内容、例えば、複雑な文章；特殊符号など</param>
+        /// <returns>影響された記録の数</returns>
+        public static int ExecuteSql(string SQLString , string content)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(SQLString, connection);
+                MySqlParameter myParameter = new MySqlParameter("@content", SqlDbType.NText);
+                myParameter.Value = content;
+                cmd.Parameters.Add(myParameter);
+                try
+                {
+                    connection.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows;
+                }
+                catch(MySqlException e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// ⑤保存した引数付きのSQLを実行
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="content"></param>
+        /// <returns>影響された記録の数</returns>
+        public static object ExecuteSqlGet(string SQLString , string content)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(SQLString, connection);
+                MySqlParameter myParameter = new MySqlParameter("@content", SqlDbType.NText);
+                myParameter.Value = content;
+                cmd.Parameters.Add(myParameter);
+                try
+                {
+                    connection.Open();
+                    object obj = cmd.ExecuteScalar();
+                    if((Object.Equals(obj,null)) || (Object.Equals(obj,System.DBNull.Value)))
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return obj;
+                    }
+                }
+                catch(MySqlException e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// ⑥DBの中にIMGを入れる
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <param name="fs">imageを入れる</param>
+        /// <returns></returns>
+        public static int ExecuteSqlInsertImg(string strSQL,byte[] fs)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(strSQL, connection);
+                MySqlParameter myParameter = new MySqlParameter("@fs", SqlDbType.Image);
+                myParameter.Value = fs;
+                cmd.Parameters.Add(myParameter);
+                try
+                {
+                    connection.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows;
+                }
+                catch(MySqlException e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// ⑦メソッド一つ実行すると、Object結果を戻す
         /// </summary>
         /// <param name="SQLString"></param>
         /// <returns>検査結果(object)</returns>
@@ -159,12 +353,185 @@ namespace Maticsoft.DBUtility
             }
         }
 
+        /// <summary>
+        /// ⑧検索実行した後、MySqlDataReaderを戻す
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns></returns>
+        public static MySqlDataReader ExecuteReader(string strSQL)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand(strSQL, connection);
+            try
+            {
+                connection.Open();
+                MySqlDataReader myReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                return myReader;
+            }
+            catch(MySqlException e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// ⑨検索実行した後、DataSetを戻す
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <returns></returns>
+        public static DataSet Query(string SQLString)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                DataSet ds = new DataSet();
+                try
+                {
+                    connection.Open();
+                    MySqlDataAdapter command = new MySqlDataAdapter(SQLString, connection);
+                    command.Fill(ds, "ds");
+                }
+                catch(MySqlException ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                return ds;
+            }
+        }
+
+        public static DataSet Query(string SQLString , int Times)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                DataSet ds = new DataSet();
+                try
+                {
+                    connection.Open();
+                    MySqlDataAdapter command = new MySqlDataAdapter(SQLString, connection);
+                    command.SelectCommand.CommandTimeout = Times;
+                    command.Fill(ds, "ds");
+                }
+                catch(MySqlException ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                return ds;
+            }
+        }
+
         #endregion
 
         #region 引数付きのSQLメソッド
 
         /// <summary>
-        /// 検索メソッド一つ実行すると、Object結果を戻す
+        /// ①SQLstringを実行、影響された記録を戻す
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="cmdParms"></param>
+        /// <returns></returns>
+        public static int ExecuteSql(string SQLString ,params MySqlParameter[] cmdParms)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    try
+                    {
+                        PrepareCommand(cmd, connection, null, SQLString, cmdParms);
+                        int rows = cmd.ExecuteNonQuery();
+                        cmd.Parameters.Clear();
+                        return rows;
+                    }
+                    catch(MySqlException e)
+                    {
+                        throw e;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ②多数SQLStringを実行
+        /// </summary>
+        /// <param name="SQLStringList"></param>
+        public static void ExecuteSqlTran(Hashtable SQLStringList)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlTransaction trans = conn.BeginTransaction())
+                {
+                    MySqlCommand cmd = new MySqlCommand();
+                    try
+                    {
+                        foreach (DictionaryEntry myDE in SQLStringList)
+                        {
+                            string cmdText = myDE.Key.ToString();
+                            MySqlParameter[] cmdParms = (MySqlParameter[])myDE.Value;
+                            PrepareCommand(cmd, conn, trans, cmdText, cmdParms);
+                            int val = cmd.ExecuteNonQuery();
+                            cmd.Parameters.Clear();
+                        }
+                        trans.Commit();
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ③多数SQLStringを実行
+        /// </summary>
+        /// <param name="SQLStringList"></param>
+        public static void ExecuteSqlTranWithIndentity(Hashtable SQLStringList)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlTransaction trans = conn.BeginTransaction())
+                {
+                    MySqlCommand cmd = new MySqlCommand();
+                    try
+                    {
+                        int indentity = 0;
+                        foreach (DictionaryEntry myDE in SQLStringList)
+                        {
+                            string cmdText = myDE.Key.ToString();
+                            MySqlParameter[] cmdParms = (MySqlParameter[])myDE.Value;
+                            foreach (MySqlParameter q in cmdParms)
+                            {
+                                if(q.Direction == ParameterDirection.InputOutput)
+                                {
+                                    q.Value = indentity;
+                                }
+                            }
+                            PrepareCommand(cmd, conn, trans, cmdText, cmdParms);
+                            int val = cmd.ExecuteNonQuery();
+                            foreach (MySqlParameter q in cmdParms)
+                            {
+                                if(q.Direction == ParameterDirection.Output)
+                                {
+                                    indentity = Convert.ToInt32(q.Value);
+                                }
+                            }
+                            cmd.Parameters.Clear();
+                        }
+                        trans.Commit();
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ④検索メソッド一つ実行すると、Object結果を戻す
         /// </summary>
         /// <param name="SQLString"></param>
         /// <param name="cmdParms"></param>
@@ -196,6 +563,58 @@ namespace Maticsoft.DBUtility
             }
         }
 
+        /// <summary>
+        /// ⑤検索メソッド一つ実行すると、MySQLDataReaderを戻す
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="cmdParms"></param>
+        /// <returns></returns>
+        public static MySqlDataReader ExecuteReader(string SQLString,params MySqlParameter[] cmdParms)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand();
+            try
+            {
+                PrepareCommand(cmd, connection, null, SQLString, cmdParms);
+                MySqlDataReader myReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                cmd.Parameters.Clear();
+                return myReader;
+            }
+            catch(MySqlException e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// ⑥検索メソッド一つ実行すると、DataSetを戻す
+        /// </summary>
+        /// <param name="SQLString"></param>
+        /// <param name="cmdParms"></param>
+        /// <returns></returns>
+        public static DataSet Query(string SQLString,params MySqlParameter[] cmdParms)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                PrepareCommand(cmd, connection, null, SQLString, cmdParms);
+                using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    try
+                    {
+                        da.Fill(ds, "ds");
+                        cmd.Parameters.Clear();
+                    }
+                    catch(MySqlException ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    return ds;
+                }
+            }
+        }
+
         private static void PrepareCommand(MySqlCommand cmd, MySqlConnection conn , MySqlTransaction trans , string cmdText, MySqlParameter[] cmdParms)
         {
             if (conn.State != ConnectionState.Open) conn.Open();
@@ -218,7 +637,6 @@ namespace Maticsoft.DBUtility
         }
 
         #endregion
-
 
     }
 }
